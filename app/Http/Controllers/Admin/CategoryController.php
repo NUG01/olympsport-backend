0cc1651a36e6247b8bc6df8c48504e364201ed0b
nuguntu <nskhiereli@gmail.com>
2023-04-25 12:32:17 +0400

    refactor

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -50,7 +50,7 @@ class CategoryController extends Controller
     public function search(Request $request)
     {
 
-        $categories = Category::where('name', 'like', $request->name . '%')->get();
+        $categories = Category::where('name', 'like', '%' . $request->name . '%')->get();
         return response()->json(['data' => $categories]);
     }
 }
