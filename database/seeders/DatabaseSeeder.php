40b94948760f11d71342963118bf8672742f2662
Vladimer Keinashvili <vovakeinashvili@gmail.com>
2023-04-27 00:23:38 +0400

    Added dummy seeder for second user + dummy payment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        $user = \App\Models\User::factory()->create([
+        $user1 = \App\Models\User::factory()->create([
             'first_name' => 'admin',
             'last_name' => 'adminiani',
             'username' => 'admina69',
@@ -30,6 +30,20 @@ class DatabaseSeeder extends Seeder
             'role' => Role::ADMIN->value,
             'city' => 1,
             'stripe_id' => 'cus_NafCJZeLv7C0RZ',
+            'email_verified_at' => now(),
+        ]);
+
+        $user2 = \App\Models\User::factory()->create([
+            'first_name' => 'Seeker',
+            'last_name' => 'Seekeriani',
+            'username' => 'seeker',
+            'email' => 'seeker@gmail.com',
+            'phone_number' => '+995 555 555 555',
+            'password' => bcrypt('Seeker123'),
+            'verification_code' => sha1(time()),
+            'role' => Role::SEEKER->value,
+            'city' => 1,
+            'stripe_id' => 'cus_Ngbua1uv8oXu5f',
             'email_verified_at' => now(),
         ]);
 
@@ -48,7 +62,16 @@ class DatabaseSeeder extends Seeder
         }
 
         Subscription::create([
-            'user_id' => $user->id,
+            'user_id' => $user1->id,
+            'name' => 'Online TOTAL!',
+            'stripe_id' => 'sub_1MpTs9LLLxBR1G3Rjc8L1paS',
+            'stripe_status' => 'active',
+            'stripe_price' => 'plan_N6MOhmJUwOIRQK',
+            'quantity' => 1,
+        ]);
+
+        Subscription::create([
+            'user_id' => $user2->id,
             'name' => 'Online TOTAL!',
             'stripe_id' => 'sub_1MpTs9LLLxBR1G3Rjc8L1paS',
             'stripe_status' => 'active',
@@ -69,6 +92,14 @@ class DatabaseSeeder extends Seeder
 
         DB::table('subscription_items')->insert([
             'subscription_id' => 1,
+            'stripe_id' => 'si_NafCrfqls132Qf',
+            'stripe_product' => 'prod_N6MODz7wBzQJKT',
+            'stripe_price' => 'plan_N6MOhmJUwOIRQK',
+            'quantity' => 1,
+        ]);
+
+        DB::table('subscription_items')->insert([
+            'subscription_id' => 2,
             'stripe_id' => 'si_NafCrfqls132Qf',
             'stripe_product' => 'prod_N6MODz7wBzQJKT',
             'stripe_price' => 'plan_N6MOhmJUwOIRQK',
